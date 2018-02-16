41b1f61f365e355e4590a2873ade7753df1e5a30
Vivek Shukla <viv3kshukla@gmail.com>
2018-02-16 16:59:52 +0530

    create income add view (only get)

--- income/views.py
+++ income/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views import View
 
 from .models import Income, Source
+from .forms import IncomeForm
 # Create your views here.
 
 
@@ -12,3 +14,23 @@ class IncomeList(ListView):
     model = Income
     paginate_by = 15
     context_object_name = 'objects'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = 'Income List'
+        return context
+
+
+class IncomeAdd(View):
+
+    template_name = 'add_income.html'
+    form_class = IncomeForm
+
+    def get(self, request, *args, **kwargs):
+
+        context = {
+            'income_form': self.form_class,
+            'title' : 'Add Income',
+        }
+
+        return render(request, self.template_name, context)
