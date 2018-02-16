4ba005c8de5b65b5850bafb957f3a51f296cc723
Vivek Shukla <viv3kshukla@gmail.com>
2018-02-16 15:39:33 +0530

    create IncomeList view

--- income/views.py
+++ income/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from .models import Income, Source
 # Create your views here.
+
+
+
+class IncomeList(ListView):
+
+    template_name = 'income_list.html'
+    model = Income
+    paginate_by = 15
+    context_object_name = 'objects'
