3dce113673592c43aa04ffa0e4cd90753bbe5512
Vivek Shukla <viv3kshukla@gmail.com>
2018-02-16 15:38:32 +0530

    create models for income app

--- income/models.py
+++ income/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+
+class Source(models.Model):
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    
+
+class Income(models.Model):
+
+    user = models.ForeignKey(User)
+    amount = models.PositiveIntegerField()
+    source = models.ForeignKey(Source, blank=True, null=True)
+    timestamp = models.DateField()
+
+    def __str__(self):
+        return "{} : {}".format(self.user, self.source)
