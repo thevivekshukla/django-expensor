9dc3c8910a9fe23368e8523105c95f57c8ecd8bf
Vivek Shukla <viv3kshukla@gmail.com>
2018-02-22 13:07:29 +0530

    change database settings in old_settings.py

--- expensor/old_settings.py
+++ expensor/old_settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'expensor.wsgi.application'
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DB_NAME', ''),
-#         'USER': os.environ.get('DB_USER', ''),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-#         'HOST': os.environ.get('DB_HOST', ''),
-#         'PORT': os.environ.get('DB_PORT', ''),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
 
