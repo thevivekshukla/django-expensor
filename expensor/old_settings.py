5b86e8fb88a9a5c3e917cf6e6a6d32417f730615
Vivek Shukla <viv3kshukla@gmail.com>
2018-03-16 00:49:28 +0530

    update static root directory name

--- expensor/old_settings.py
+++ expensor/old_settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     STATIC_DIR,
