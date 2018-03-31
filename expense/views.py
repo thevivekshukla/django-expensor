9196de70ec21b5ab0f8736c60cdbfcf370663ac8
Vivek Shukla <viv3kshukla@gmail.com>
2018-03-31 15:36:40 +0530

    minor changes

--- expense/views.py
+++ expense/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.utils import timezone
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Sum
