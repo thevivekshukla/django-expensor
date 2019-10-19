e3aa118e7ccafe2a91eb8955a44e082073ba91ad
duaboola <50785323+duaboola@users.noreply.github.com>
2019-10-19 14:21:04 +0530

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
         url(r'^login/$', views.user_login, name='login'),
         url(r'^logout/$', views.user_logout, name='logout'),
         url(r'^change-password/$', views.ChangePassword.as_view(), name='change-password'),
+        url(r'^table/$', views.Table.as_view(), name='table'),
 ]
