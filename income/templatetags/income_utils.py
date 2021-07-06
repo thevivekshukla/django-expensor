6e2e3b1f9f88682a627a8c4f70cca8d62adc978c
Vivek Shukla <viv3kshukla@gmail.com>
2021-07-06 21:37:15 +0530

    add logic to show calculator

---
+++ income/templatetags/income_utils.py
@@ -0,0 +1,15 @@
+from datetime import timedelta, timezone
+
+from django.utils import timezone
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='show_calculator')
+def show_calculator(income_object):
+    now = timezone.now()
+    later_created_time = income_object.created_at + timedelta(days=7)
+    if now < later_created_time:
+        return True
+    return False
