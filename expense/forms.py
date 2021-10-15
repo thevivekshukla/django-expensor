516afb03baa53f3a627340483588d103ecba45cc
Vivek Shukla <viv3kshukla@gmail.com>
2021-10-16 02:19:53 +0530

    remove unnecessary commented code

--- expense/forms.py
+++ expense/forms.py
@@ -16,22 +16,6 @@ class ExpenseForm(forms.Form):
         super().__init__(*args, **kwargs)
         self.fields['timestamp'].initial = get_ist_datetime().date()
 
-    # def clean_timestamp(self, *args, **kwargs):
-    #    timestamp = self.cleaned_data.get('timestamp')
-    #    today = date.today()
-        # three_day = today - timedelta(days=3)
-        # ten_day = today - timedelta(days=10)
-    #    thirty_day = today - timedelta(days=30)
-
-    #    if timestamp > today:
-    #        raise forms.ValidationError("Date cannot be set in future.")
-
-    #    if timestamp < thirty_day:
-    #        raise forms.ValidationError("Date cannot be set more than 30 days in past.")
-
-    #   return timestamp
-
-
 
 class SelectDateExpenseForm(forms.Form):
     remark = forms.CharField(required=False,
