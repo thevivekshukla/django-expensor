1a576d139a7405f00c1941e4ed2c70eea595f873
Vivek Shukla <viv3kshukla@gmail.com>
2021-11-01 18:26:46 +0530

    change no of days

--- income/templatetags/income_utils.py
+++ income/templatetags/income_utils.py
@@ -10,7 +10,7 @@ register = template.Library()
 @register.filter(name='show_calculator')
 def show_calculator(income_object):
     now = helpers.get_ist_datetime()
-    later_created_time = income_object.created_at + timedelta(days=3)
+    later_created_time = income_object.created_at + timedelta(days=2)
     if now < later_created_time:
         return True
     return False
