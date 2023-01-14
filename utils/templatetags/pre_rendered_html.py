7f9fbc06e471ec3fab4746a5c31c3f34d6d91e3c
Vivek Shukla <viv3kshukla@gmail.com>
2023-01-15 02:27:24 +0530

    revert

--- utils/templatetags/pre_rendered_html.py
+++ utils/templatetags/pre_rendered_html.py
@@ -10,7 +10,7 @@ def past_days_search():
     html = ['<ul class="pager">']
     for days in [30, 90, 180, 365]:
         html.append(
-            f'<li><a href="?from_date={helpers.timedelta_now_str(days)}'
+            f'<li><a href="?from_date={helpers.timedelta_now_str(days-1)}'
             f'&to_date={helpers.timedelta_now_str(0)}">{days}</a></li>'
         )
     html.append('</ul>')
