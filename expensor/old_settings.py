133138512494c61e5631d623cbdd1fd21ebb4310
Vivek Shukla <viv3kshukla@gmail.com>
2023-04-14 03:39:57 +0530

    remote gtag script

--- expensor/old_settings.py
+++ expensor/old_settings.py
@@ -87,7 +87,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "utils.context_processors.gtag",
                 "utils.context_processors.constants",
             ],
         },
--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,12 +1,7 @@
 from decouple import config
-from .constants import *
 
-def gtag(request):
-    return {'gtag': config('GTAG_ID', '')}
+from .constants import *
 
 
 def constants(request):
-    return {
-        'constants': {k:v for k, v in globals().items() if k.isupper()}
-    }
-
+    return {"constants": {k: v for k, v in globals().items() if k.isupper()}}
