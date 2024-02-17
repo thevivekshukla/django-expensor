74382da4d91d9f8f7226d7611b26448357045a0b
Vivek Shukla <viv3kshukla@gmail.com>
2024-02-18 02:23:15 +0530

    using gevent worker class

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,10 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = (2 * multiprocessing.cpu_count()) + 1
+worker_class = "gevent"
 accesslog = "-"
 loglevel = "error"
 forwarded_allow_ips = "*"
 proxy_allow_ips = "*"
 proxy_protocol = True
+max_requests = 128
